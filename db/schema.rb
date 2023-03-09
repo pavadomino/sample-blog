56da50750fca9cee8ea3fc7e120d805873787e39
pavadomino <pavadomino@gmail.com>
2023-03-09 18:43:58 +0100

    Create Contact model and update view

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_213312) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_173536) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
