752a93514c9d75dcf06ce6ef3912b1297a717ccb
pavadomino <pavadomino@gmail.com>
2023-03-11 15:37:00 +0100

    Add index view for articles and display all articles on the index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,21 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new'
+    end
+  end
+
+  def index
+    @list = Article.order "created_at DESC"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
