659ff326591793dfd7f2eb90fb0da811cf531b2d
pavadomino <pavadomino@gmail.com>
2023-03-11 14:38:18 +0100

    Add terms and about controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get '/about' => 'about#show'
 
+  resource :terms, only: [:show]
   resources :articles
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
