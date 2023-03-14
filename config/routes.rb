d4c549614e9971205f165913003cf2bb881bf5bc
pavadomino <pavadomino@gmail.com>
2023-03-14 19:45:07 +0100

    Minor improvements to routes and show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get '/about' => 'about#show'
+  get 'about' => 'about#show'
 
   resource :terms, only: [:show]
   resources :articles do
-    resources :comments
+    resources :comments, only: [:create]
   end
   resource :contacts, only: [:new, :create]#, path_names: { :new => '' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
