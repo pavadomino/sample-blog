f93bb4b863cde34e17b17ae030d712cf7af62c1b
pavadomino <pavadomino@gmail.com>
2023-03-22 21:08:21 +0100

    Authenticate user for specific actions update, destroy, create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_article, only: [:edit, :show, :update, :destroy]
 
   def new
