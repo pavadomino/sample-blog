7c3f15467cc941b68ab8e052e0f427f38e87193e
pavadomino <pavadomino@gmail.com>
2023-03-22 20:15:39 +0100

    Permit username for sign_up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success, :messages
+  before_action :configure_permitted_parameters, if: :devise_controller? do
+
+    private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitazer.for(:sign_up) << :username
+      #devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+  end
 end
