552ce6d1767142a5975105aac0966ca9c1139ba6
pavadomino <pavadomino@gmail.com>
2023-04-01 00:01:07 +0200

    Add article spec with validations and associations

--- spec/model/article_spec.rb
+++ spec/model/article_spec.rb
@@ -2,5 +2,12 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it { should have_many :comments }
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+  describe 'associations' do
+    it { should have_many :comments }
+  end
 end
