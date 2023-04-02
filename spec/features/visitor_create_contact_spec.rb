1550fbb72e71cbffc22148ee33d8f9a876ddc63b
pavadomino <pavadomino@gmail.com>
2023-04-02 23:26:37 +0200

    lesson 47. Fix create contact acceptance test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 feature "Contact Creation" do
   scenario "allow access to contacts page" do
     visit new_contacts_path
-    expect(page).to have_content 'Contact us'
+
+    expect(page).to have_content I18n.t('contacts.contact_us')
+  end
+
+  scenario "allow a guest to create contact" do
 
   end
 end
